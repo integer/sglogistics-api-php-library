87c9c7c457bb16a618f4ab94c72bf5b503f944cf
Ondrej Nespor <ondrej.nespor@slevomat.cz>
2012-10-10 15:05:02 +0200

    Version bump

--- SgLogistics/Client.php
+++ SgLogistics/Client.php
@@ -4,8 +4,8 @@
  * SG Logistics client API
  *
  * @copyright Copyright (c) 2012 Slevomat.cz, s.r.o.
- * @version 0.9
- * @apiVersion 0.9
+ * @version 1.0
+ * @apiVersion 1.0
  */
 
 namespace SgLogistics\Api;
