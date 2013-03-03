10391dd8de4f2bd9326a9e442d799c72b8d51e46
Ondrej Nespor <ondrej.nespor@slevomat.cz>
2013-03-03 20:23:10 +0100

    Exception thrown when the SGL system is down for maintenance

---
+++ SgLogistics/Exception/Maintenance.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * SG Logistics client API
+ *
+ * @copyright Copyright (c) 2012-2013 Slevomat.cz, s.r.o.
+ * @version 1.9
+ * @apiVersion 1.0
+ */
+
+namespace SgLogistics\Api\Exception;
+
+/**
+ * Exception thrown when the SGL system is down for maintenance.
+ *
+ * @category SgLogistics
+ * @package  Api
+ */
+class Maintenance extends Response
+{
+	/**
+	 * Creates the exception.
+	 */
+	public function __construct()
+	{
+		parent::__construct('The SGL system is down for maintenance.');
+	}
+}
