70a518d686f195aba2c7bc22bb98a2f97ce82737
Ondrej Nespor <ondrej.nespor@slevomat.cz>
2013-04-17 13:48:14 +0200

    Version bump

--- SgLogistics/Protocol/Rest.php
+++ SgLogistics/Protocol/Rest.php
@@ -4,8 +4,8 @@
  * SG Logistics client API
  *
  * @copyright Copyright (c) 2012-2013 Slevomat.cz, s.r.o.
- * @version 1.11
- * @apiVersion 1.0
+ * @version 1.12
+ * @apiVersion 1.1
  */
 
 namespace SgLogistics\Api\Protocol;
