c97512b437dc907914220408efe3a2eba134bea0
Ondrej Nespor <ondrej.nespor@slevomat.cz>
2014-08-06 15:24:34 +0200

    Vesion bump

--- SgLogistics/Client.php
+++ SgLogistics/Client.php
@@ -4,7 +4,7 @@
  * SG Logistics client API
  *
  * @copyright Copyright (c) 2012-2013 Slevomat.cz, s.r.o.
- * @version 1.26
+ * @version 1.27
  * @apiVersion 1.2
  */
 
@@ -23,7 +23,7 @@ class Client
 	 *
 	 * @var string
 	 */
-	const VERSION = '1.26';
+	const VERSION = '1.27';
 
 	/**
 	 * URL of the Github repository.
